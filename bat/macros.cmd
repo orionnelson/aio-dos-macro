8ad0ba1c5d4bd4287a2c673241b4bc167bc5844c
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-06-30 19:08:45 -0400

    Update macros.cmd

--- bat/macros.cmd
+++ bat/macros.cmd
@@ -3,3 +3,5 @@ DOSKEY ls=dir /B $1
 DOSKEY pmax=liquidctl --match kraken set pump speed 100
 DOSKEY pmin=liquidctl --match kraken set pump speed 40
 DOSKEY pump=liquidctl --match kraken set pump speed $*
+DOSKEY loff=liquidctl --match kraken set sync color off
+DOSKEY lon=liquidctl --match kraken set sync color super-rainbow --direction backward --speed faster
