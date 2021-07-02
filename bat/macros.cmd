c40be452b84ddd64f21b7844692f87c93ff930d2
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-07-02 14:44:31 -0400

    Update macros.cmd

--- bat/macros.cmd
+++ bat/macros.cmd
@@ -3,5 +3,5 @@ DOSKEY ls=dir /B $1
 DOSKEY pmax=liquidctl --match kraken set pump speed 100
 DOSKEY pmin=liquidctl --match kraken set pump speed 40
 DOSKEY pump=liquidctl --match kraken set pump speed $*
-DOSKEY loff=liquidctl --match kraken set sync color off
+DOSKEY loff=taskkill /F /IM SignalRgb.exe && liquidctl --match kraken set sync color off
 DOSKEY lon=liquidctl --match kraken set sync color fixed ff0000 && start "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\WhirlwindFX\SignalRgb.lnk" && timeout /t 3 && taskkill /im SignalRgb.exe
